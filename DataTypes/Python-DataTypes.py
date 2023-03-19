41a6adbd928ec71ec0804e86c97dfc436432dd60
Adem Furkan Kavuş <150111027@ogrenci.yalova.edu.tr>
2023-03-19 15:02:02 +0300

    First Commit

---
+++ DataTypes/Python-DataTypes.py
@@ -0,0 +1,30 @@
+# String Data Type
+a = "String Veri"
+
+# Integer Data Type
+b = 26
+
+# Float Data Type
+c = 26.5
+
+#Complex Data Type
+d = 5 + 3j
+
+#List Data Type
+e = ["List","Data","Types"]
+
+#Tuple Data Type
+f = ("Tuple", "Data", "Types")
+
+#Dictionary Data Type
+g = {"name" : "Ali", "age" : 26}
+
+#Set Data Type
+h = {"Set", "Data", "Types"}
+
+#Boolean Data Type
+k = True
+
+#Bytes in Binary Data Type
+l = b"Binary"
+
---
+++ DataTypes/Python-Kodlamaio-DataTypes.py
@@ -0,0 +1,14 @@
+kodlamaio_courses = ["Yazılım Geliştirici Yetiştirme Kampı (JAVA + REACT)","(2022) Yazılım Geliştirici Yetiştirme Kampı -JAVA","Yazılım Geliştirici Yetiştirme Kampı (JavaScript)","Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR)","(2023) Yazılım Geliştirici Yetiştirme Kampı - Python & Selenium","Senior Yazılım Geliştirici Yetiştirme Kampı (.NET)","Programlamaya Giriş Temel Kurs"]
+
+for course in kodlamaio_courses:
+    print(course)
+
+user_name = "hikmet15"
+user_password = 123456
+log_in = bool
+
+
+if (user_name == "hikmet15" and user_passworc == 123456):
+    log_in = True
+else:
+    log_in = False
